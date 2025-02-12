76f1e9e65ccb3b7ff7b251ca81caa3d09703d83a
EvaldoBritoJunior <evaldo.brito.junior@ccc.ufcg.edu.br>
2025-02-12 09:12:17 -0300

    feat: Add Engine

---
+++ grimoire-backend/src/main/java/com/grimoire/repository/EngineRepository.java
@@ -0,0 +1,15 @@
+package com.grimoire.repository;
+
+import com.grimoire.model.grimoire.EngineModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EngineRepository extends JpaRepository<EngineModel, String> {
+
+    Optional<EngineModel> findByName(String name);
+    boolean existsByName(String name);
+    boolean isPublic(String name);
+}
